b2117b344428238eb74c932684d6678e4cae646e
MattAndrzejczuk <cinicraft@me.com>
2021-02-11 08:29:19 -0500

    Initial Commit

---
+++ CodingChallenge_SwiftUI/CodingChallenge_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingChallenge_SwiftUIApp.swift
+//  CodingChallenge_SwiftUI
+//
+//  Created by Matt Andrzejczuk on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CodingChallenge_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
